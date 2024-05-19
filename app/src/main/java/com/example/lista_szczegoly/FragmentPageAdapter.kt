d5f267cc180b905fd11dcaf244d5e7e3a4f591da
MagnuSiwy <pankacper890@gmail.com>
2024-05-19 14:56:01 +0200

    Fixed run errors

--- app/src/main/java/com/example/lista_szczegoly/FragmentPageAdapter.kt
+++ app/src/main/java/com/example/lista_szczegoly/FragmentPageAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class FragmentPageAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getCount(): Int {
         return 3
     }
